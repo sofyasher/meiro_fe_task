809851c0dd9baee39e5ee99a84f81e1260d8c827
sofie <sophiess@seznam.cz>
2024-07-08 17:51:01 +0200

    navigation added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
 import NotFound from './pages/not-found/not-found';
 import AttributeList from './pages/attribute-list/attribute-list';
 import AttributeDetail from './pages/attribute-detail/attribute-detail';
 import Home from './pages/home/home';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/attributes',
-    element: <AttributeList />,
-  },
-  {
-    path: '/attributes/:id',
-    element: <AttributeDetail />,
-  },
-  {
-    path: '*',
-    element: <NotFound />,
-  },
-]);
-
+const Navigation = () => {
+  return (
+    <Navbar expand='lg' className='bg-body-tertiary'>
+      <Container fluid>
+        <Navbar.Brand href='/'>Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls='navbarScroll' />
+        <Navbar.Collapse id='navbarScroll'>
+          <Nav
+            className='me-auto my-2 my-lg-0'
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href='/attributes'>Attributes</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 const App = () => {
   return (
     <div className='App'>
-      <RouterProvider router={router} />
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/attributes' element={<AttributeList />} />
+        <Route path='/attributes/:id' element={<AttributeDetail />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
